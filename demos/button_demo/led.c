db21f53aef63ee58ca3a55d61d898d6940aa7cff
student <student@systems-vm.lan>
2021-03-24 13:25:11 -0600

    added p2 button function to blink demo

--- demos/button_demo/led.c
+++ demos/button_demo/led.c
@@ -10,41 +10,11 @@ void led_init()
 }
 
 void led_update(){
-  if (switch_state_changed && switch_state_down[0]) {
+  if (switch_state_changed) {
     char ledFlags = 0; /* by default, no LEDs on */
 
     ledFlags |= switch_state_down[0] ? LED_GREEN : 0;
     ledFlags |= switch_state_down[0] ? 0 : LED_RED;
-
-    P1OUT &= (0xff - LEDS) | ledFlags; // clear bits for off leds
-    P1OUT |= ledFlags;         // set bits for on leds
-  }
-  
-  if (switch_state_changed && switch_state_down[1]) {
-    char ledFlags = 0; /* by default, no LEDs on */
-
-    ledFlags |= switch_state_down[1] ? LED_GREEN : 0;
-    ledFlags |= switch_state_down[1] ? LED_RED : 0;
-
-    P1OUT &= (0xff - LEDS) | ledFlags; // clear bits for off leds
-    P1OUT |= ledFlags;         // set bits for on leds
-  }
-  
-  if (switch_state_changed && switch_state_down[2]) {
-    char ledFlags = 0; /* by default, no LEDs on */
-
-    ledFlags |= switch_state_down[2] ? 0 : LED_GREEN;
-    ledFlags |= switch_state_down[2] ? LED_RED : 0;
-
-    P1OUT &= (0xff - LEDS) | ledFlags; // clear bits for off leds
-    P1OUT |= ledFlags;         // set bits for on leds
-  }
-  
-  if (switch_state_changed && switch_state_down[3]) {
-    char ledFlags = 0; /* by default, no LEDs on */
-
-    ledFlags |= switch_state_down[3] ? 0 : LED_GREEN;
-    ledFlags |= switch_state_down[3] ? 0 : LED_RED;
 
     P1OUT &= (0xff - LEDS) | ledFlags; // clear bits for off leds
     P1OUT |= ledFlags;         // set bits for on leds
