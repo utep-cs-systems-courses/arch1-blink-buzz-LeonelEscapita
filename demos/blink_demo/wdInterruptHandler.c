07477b4a81909aecc7e258dcee5ad99b7da7f22e
student <student@systems-vm.lan>
2021-03-26 13:05:24 -0600

    fixed button demo, onto buzzer

--- demos/blink_demo/wdInterruptHandler.c
+++ demos/blink_demo/wdInterruptHandler.c
@@ -4,13 +4,15 @@
 void
 __interrupt_vec(WDT_VECTOR) WDT(){	/* 250 interrupts/sec */
   static char blink_count = 0;
+  static char blink_count2 = 0;
+  
   if (++blink_count == 50) {
     state_advance();
-    //blink_count = 0;
-  }
-  if (++blink_count == 90) {
-    state_advance_sl();
     blink_count = 0;
+  }
+  if (++blink_count2 == 130) {
+    state_advance_sl();
+    blink_count2 = 0;
   }
   
 }
