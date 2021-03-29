ba26cadfa42da6c3dab871b7a6d91fd5d01d91b6
student <student@systems-vm.lan>
2021-03-28 21:31:21 -0600

    buttons work all instructions from readme

--- demos/buzzer_demo/stateMachines.c
+++ demos/buzzer_demo/stateMachines.c
@@ -73,76 +73,103 @@ char toggle_green2()
   return 1;
 }
 
-
-void state_advance1()		/* alternate between toggling red & green */
-{
-  char changed = 0;  
-  static char state = 0;
-  
-  switch(state) {
-   case 0: changed = toggle_red(); state = 1; break;
-   case 1: changed = toggle_green(); state = 0; break;
-  }
-
-  led_changed = changed;
-  led_update();
-}
-
-void state_advance2()		/* alternate between toggling red & green */
-{
-  char changed = 0;  
-
-  static char state = 0;
-  
-  switch(state) {
-   case 0: changed = toggle_red2(); state = 1; break;
-   case 1: changed = toggle_green2(); state = 0; break;
-  }
-
-  led_changed = changed;
-  led_update();
-}
-
 void state_advance()
 {
   if(switch_state_down[0]) {
-    state_advance1();
+     char changed = 0;  
+     static char state = 0;
+  
+     switch(state) {
+       case 0: changed = toggle_red(); state = 1; break;
+       case 1: changed = toggle_green(); state = 0; break;
+  }
+
+   led_changed = changed;
+   led_update();
   }
   else if(switch_state_down[1]) {
-    state_advance2();
+    char changed = 0;  
+    static char state = 0;
+  
+    switch(state) {
+      case 0: changed = toggle_red2(); state = 1; break;
+      case 1: changed = toggle_green2(); state = 0; break;
+  }
+
+  led_changed = changed;
+  led_update();
   }
 }
 
 void state_advance_sl()
 {
   if(switch_state_down[2]) {
-    state_advance1();
+     char changed = 0;  
+     static char state = 0;
+  
+    switch(state) {
+      case 0: changed = toggle_red(); state = 1; break;
+      case 1: changed = toggle_green(); state = 0; break;
+  }
+
+  led_changed = changed;
+  led_update();
   }
   else if(switch_state_down[3]) {
-    state_advance2();
+    char changed = 0;  
+    static char state = 0;
+  
+    switch(state) {
+      case 0: changed = toggle_red2(); state = 1; break;
+      case 1: changed = toggle_green2(); state = 0; break;
+  }
+
+  led_changed = changed;
+  led_update();
   }
 }
 
 void state_advance_buzzer()
 {
   static char buzz_state = 0;
-
-  if(switch_state_down[0]) {
-  switch(buzz_state) {
-  case 0: buzzer_set_period(3822); buzz_state++; break;
-  ocase 1: buzzer_set_period(3405); buzz_state++; break;
-  case 2: buzzer_set_period(3033); buzz_state = 0; break;
+  if(switch_state_down[0]) { //C MAJOR
+   switch(buzz_state) {
+    case 0: buzzer_set_period(3822); buzz_state++; break;
+    case 1: buzzer_set_period(3405); buzz_state++; break;
+    case 2: buzzer_set_period(3033); buzz_state = 0; break;
   default: buzz_state = 0;
+   }
   }
+  
+  else if(switch_state_down[1]) { //F MAJOR
+  switch(buzz_state) {
+    case 0: buzzer_set_period(5727); buzz_state++; break;
+    case 1: buzzer_set_period(4545); buzz_state++; break;
+    case 2: buzzer_set_period(3822); buzz_state = 0; break;
+  default: buzz_state = 0;
+   }
   }
+
+  else if(switch_state_down[2]) { // Bb MAJOR
+  switch(buzz_state) {
+    case 0: buzzer_set_period(4290); buzz_state++; break;
+    case 1: buzzer_set_period(3407); buzz_state++; break;
+    case 2: buzzer_set_period(2863); buzz_state = 0; break;
+  default: buzz_state = 0;
+   }
+  }
+
+  else if(switch_state_down[3]) { // F# MAJOR
+  switch(buzz_state) {
+    case 0: buzzer_set_period(5410); buzz_state++; break;
+    case 1: buzzer_set_period(4290); buzz_state++; break;
+    case 2: buzzer_set_period(3607); buzz_state = 0; break;
+  default: buzz_state = 0;
+   }
+  }
+  
   else {
     buzzer_set_period(0);
   }
 
 }
-
-
-
-
-
-
